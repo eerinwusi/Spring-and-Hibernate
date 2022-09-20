e2bcefc4f77ae6ec951489ac49f4275d80776769
Emmanuel Erinwusi <Emmanuel.Erinwusi@etranzact.com>
2022-09-20 14:25:14 +0100

    @autowired annotation using constructor injection

--- spring-demo-annotations/src/com/springndhibernate/springdemo/FortuneService.java
+++ spring-demo-annotations/src/com/springndhibernate/springdemo/FortuneService.java
@@ -1,5 +1,7 @@
 package com.springndhibernate.springdemo;
 
 public interface FortuneService {
+	
+	public String getFortune();
 
 }
--- spring-demo-annotations/src/com/springndhibernate/springdemo/HappyFortuneService.java
+++ spring-demo-annotations/src/com/springndhibernate/springdemo/HappyFortuneService.java
@@ -1,11 +1,15 @@
 package com.springndhibernate.springdemo;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class HappyFortuneService implements FortuneService {
 
 	@Override
 	public String getFortune() {
 		// TODO Auto-generated method stub
-		return null;
+		return "Today is your lucky day";
 	}
+
 
 }
