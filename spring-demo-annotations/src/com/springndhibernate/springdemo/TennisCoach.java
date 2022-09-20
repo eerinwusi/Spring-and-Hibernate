b7e8032926813f071d4cfab65a1df88750e523f0
Emmanuel Erinwusi <Emmanuel.Erinwusi@etranzact.com>
2022-09-20 15:44:08 +0100

    prototype scope using annotation

--- spring-demo-annotations/src/com/springndhibernate/springdemo/TennisCoach.java
+++ spring-demo-annotations/src/com/springndhibernate/springdemo/TennisCoach.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
-@Scope(value = "singleton")
+@Scope(value = "prototype")
 public class TennisCoach implements Coach {
 	
 	@Autowired
