bb1e6521382278a9f83cf6196c3e57c0bfd67d77
Emmanuel Erinwusi <Emmanuel.Erinwusi@etranzact.com>
2022-09-20 12:04:17 +0100

    Bean Scope (Singleton)

---
+++ spring-demo-one/src/com/springndhibernate/springdemo/BeansScopeDemoApp.java
@@ -0,0 +1,28 @@
+package com.springndhibernate.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class BeansScopeDemoApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beanScope-applicationContext.xml");
+		
+		Coach theCoach = context.getBean("myCoach", Coach.class);
+		
+		Coach alphaCoach = context.getBean("myCoach", Coach.class);
+		
+//		check if they are the same
+		boolean result = (theCoach == alphaCoach);
+		
+//		print results
+		System.out.println("\n Pointing to the same object" + result);
+		
+		System.out.println("\n Memory location for theCoach" + theCoach);
+		
+		System.out.println("\n Memory location for alphaCoach" + alphaCoach);
+
+	}
+
+}
