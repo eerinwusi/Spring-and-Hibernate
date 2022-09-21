1ec5652fc6ab9e9a9662f68eb5e5679562c4ebcf
Emmanuel Erinwusi <Emmanuel.Erinwusi@etranzact.com>
2022-09-21 16:09:33 +0100

    Custom annotation done - corrected constraint validator class

--- spring-mvc-demo/src/com/luv2code/springdemo/mvc/validation/CourseCodeConstraintValidator.java
+++ spring-mvc-demo/src/com/luv2code/springdemo/mvc/validation/CourseCodeConstraintValidator.java
@@ -1,5 +1,31 @@
 package com.luv2code.springdemo.mvc.validation;
 
-public class CourseCodeConstraintValidator {
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
 
+public class CourseCodeConstraintValidator implements ConstraintValidator<CourseCode, String>{
+
+	private String coursePrefix;
+	
+	@Override
+	public void initialize(CourseCode constraintAnnotation) {
+		// TODO Auto-generated method stub
+		coursePrefix = constraintAnnotation.value();
+	}
+
+	@Override
+	public boolean isValid(String theCode, ConstraintValidatorContext arg1) {
+		
+		boolean result;
+		
+		if (theCode != null) {
+			result = theCode.startsWith(coursePrefix);
+		} else {
+			result = true;
+		}
+		// TODO Auto-generated method stub
+		return result;
+	}
+
+	
 }
