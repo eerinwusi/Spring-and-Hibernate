181be4ae8806f4d8a8d7fffb71409e26fc507de6
Emmanuel Erinwusi <Emmanuel.Erinwusi@etranzact.com>
2022-09-21 19:47:15 +0100

    Update Object by id with hibernate

---
+++ hibernate/src/com/luv2code/hibernate/demo/UpdateStudentDemo.java
@@ -0,0 +1,50 @@
+package com.luv2code.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.luv2code.hibernate.demo.entity.Student;
+
+public class UpdateStudentDemo {
+
+	public static void main(String[] args) {
+		
+//		create session factory
+		SessionFactory factory = new Configuration()
+								.configure("hibernate.cfg.xml")
+								.addAnnotatedClass(Student.class)
+								.buildSessionFactory();
+		
+
+		
+		try {
+			
+			int studentId = 1;
+			
+//			get a new session and start transaction
+			Session session = factory.getCurrentSession();
+			session.beginTransaction();
+			
+//			retrieve student by id
+			System.out.println("Getting student with id: " + studentId);
+			
+			Student myStudent = session.get(Student.class, studentId);
+			
+			System.out.println("Updating student");
+			myStudent.setFirstName("Scooby");
+			
+//			no need to save object again since it has been saved, just commit
+//			commit the transaction
+			session.getTransaction().commit();
+			
+			System.out.println("Done!");
+			
+			
+		} finally {
+			factory.close();
+		}
+
+	}
+
+}
