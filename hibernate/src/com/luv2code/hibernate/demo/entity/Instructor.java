8234d0cbd3fc525e2b74dd0c41f1d06f4fb0bb23
Emmanuel Erinwusi <Emmanuel.Erinwusi@etranzact.com>
2022-09-22 11:33:33 +0100

    Establish one-to-many relationship in Instructor class

--- hibernate/src/com/luv2code/hibernate/demo/entity/Instructor.java
+++ hibernate/src/com/luv2code/hibernate/demo/entity/Instructor.java
@@ -1,5 +1,8 @@
 package com.luv2code.hibernate.demo.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -7,6 +10,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
@@ -36,6 +40,14 @@ public class Instructor {
 	@Column(name = "first_name")
 	private String firstName;
 	
+	public List<Course> getCourses() {
+		return courses;
+	}
+
+	public void setCourses(List<Course> courses) {
+		this.courses = courses;
+	}
+
 	@Column(name = "last_name")
 	private String lastName;
 	
@@ -45,6 +57,9 @@ public class Instructor {
 	@OneToOne(cascade=CascadeType.ALL)
 	@JoinColumn(name="instructor_detail_id")
 	private InstructorDetail instructorDetail;
+	
+	@OneToMany(mappedBy="instructor", cascade= {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.DETACH, CascadeType.REFRESH})
+	private List<Course> courses;
 	
 	public Instructor() {
 		
@@ -100,6 +115,18 @@ public class Instructor {
 
 	public void setInstructorDetail(InstructorDetail instructorDetail) {
 		this.instructorDetail = instructorDetail;
+	}
+	
+//	Add convenience methods for bi-directional relationships
+	
+	public void add(Course course) {
+		if (courses == null) {
+			courses = new ArrayList<>();
+		}
+		
+		courses.add(course);
+		
+		course.setInstructor(this);
 	}
 	
 	
