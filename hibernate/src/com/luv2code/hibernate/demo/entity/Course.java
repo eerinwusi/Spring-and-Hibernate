797944b3c05f54ee10255b8f47f814b18cb8d0a6
Emmanuel Erinwusi <Emmanuel.Erinwusi@etranzact.com>
2022-09-22 15:01:14 +0100

    Configure fetch type and One-to-Many relationship

--- hibernate/src/com/luv2code/hibernate/demo/entity/Course.java
+++ hibernate/src/com/luv2code/hibernate/demo/entity/Course.java
@@ -1,13 +1,18 @@
 package com.luv2code.hibernate.demo.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -36,12 +41,24 @@ public class Course {
 	
 	private String title;
 	
+	@OneToMany(fetch=FetchType.LAZY, cascade=CascadeType.ALL)
+	@JoinColumn(name = "course_id")
+	private List<Review> reviews;
+	
 	public Course() {
 		
 	}
 
 	public Course(String title) {
 		this.title = title;
+	}
+
+	public List<Review> getReviews() {
+		return reviews;
+	}
+
+	public void setReviews(List<Review> reviews) {
+		this.reviews = reviews;
 	}
 
 	public int getId() {
@@ -71,6 +88,15 @@ public class Course {
 	@Override
 	public String toString() {
 		return "Course [id=" + id + ", title=" + title + "]";
+	}
+	
+//	add a convenience method
+	public void  addReview(Review review) {
+		if (reviews == null) {
+			reviews = new ArrayList<>();
+		}
+		
+		reviews.add(review);
 	}
 	
 	
