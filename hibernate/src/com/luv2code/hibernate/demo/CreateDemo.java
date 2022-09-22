ad39790d38704d2d539450c0c1c9be93278abce9
Emmanuel Erinwusi <Emmanuel.Erinwusi@etranzact.com>
2022-09-22 10:12:01 +0100

    Save Instructor object to db which automatically saves InstructorDetails to db because of CascadeType.ALL

--- hibernate/src/com/luv2code/hibernate/demo/CreateDemo.java
+++ hibernate/src/com/luv2code/hibernate/demo/CreateDemo.java
@@ -4,6 +4,8 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import com.luv2code.hibernate.demo.entity.Instructor;
+import com.luv2code.hibernate.demo.entity.InstructorDetail;
 import com.luv2code.hibernate.demo.entity.Student;
 
 public class CreateDemo {
@@ -13,22 +15,30 @@ public class CreateDemo {
 //		create session factory
 		SessionFactory factory = new Configuration()
 								.configure("hibernate.cfg.xml")
-								.addAnnotatedClass(Student.class)
+								.addAnnotatedClass(Instructor.class)
+								.addAnnotatedClass(InstructorDetail.class)
 								.buildSessionFactory();
 		
 //		create a session
 		Session session = factory.getCurrentSession();
 		
 		try {
-//			create the student object
-			Student student = new Student("Paul", "Will", "paul@luv2code.com");
+//			create the objects
+			Instructor instructor = new Instructor("Chad", "Darby", "darby@luv2code.com");
+			
+			InstructorDetail details = new InstructorDetail("http://www.luv2code.com/youtube", "Loves 2 code");
+			
+//			associate the objects together
+			instructor.setInstructorDetail(details);
 			
 //			start a transaction
 			session.beginTransaction();
 			
-//			save the object
-			session.save(student);
-			
+//			save the instructor. 
+//			NOTE: This will also save the details object because of the CascadeType.ALL
+			System.out.println("Saving instructor");
+			session.save(instructor);
+
 //			commit the transaction
 			session.getTransaction().commit();
 			
