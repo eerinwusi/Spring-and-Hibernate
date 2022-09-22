93f5e2902068347678ea9925d2f9c418adefcd74
Emmanuel Erinwusi <Emmanuel.Erinwusi@etranzact.com>
2022-09-22 16:21:47 +0100

    Many-To-Many - Delete Student from db

---
+++ hibernate/src/com/luv2code/hibernate/demo/DeleteMarkDemo.java
@@ -0,0 +1,54 @@
+package com.luv2code.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.luv2code.hibernate.demo.entity.Course;
+import com.luv2code.hibernate.demo.entity.Instructor;
+import com.luv2code.hibernate.demo.entity.InstructorDetail;
+import com.luv2code.hibernate.demo.entity.Review;
+import com.luv2code.hibernate.demo.entity.Student;
+
+public class DeleteMarkDemo {
+
+	public static void main(String[] args) {
+		
+//		create session factory
+		SessionFactory factory = new Configuration()
+								.configure("hibernate.cfg.xml")
+								.addAnnotatedClass(Instructor.class)
+								.addAnnotatedClass(InstructorDetail.class)
+								.addAnnotatedClass(Course.class)
+								.addAnnotatedClass(Review.class)
+								.addAnnotatedClass(Student.class)
+								.buildSessionFactory();
+		
+//		create a session
+		Session session = factory.getCurrentSession();
+		
+		try {
+			
+//			start a transaction
+			session.beginTransaction();
+
+//			get student by id
+			int theId = 2;
+			Student student = session.get(Student.class, theId);
+			
+//			delete student
+			System.out.println("Deleting...: ");
+			session.delete(student);
+
+//			commit the transaction
+			session.getTransaction().commit();
+			
+		} finally {
+			session.close();
+			
+			factory.close();
+		}
+
+	}
+
+}
