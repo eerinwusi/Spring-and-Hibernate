831939c640bd4030670452d06d39f7f4a23f98a8
Emmanuel Erinwusi <Emmanuel.Erinwusi@etranzact.com>
2022-09-23 14:09:53 +0100

    Pointcut Expressions - Match any methods with same name

--- spring-aop/src/com/luv2code/aopdemo/MainDemoApp.java
+++ spring-aop/src/com/luv2code/aopdemo/MainDemoApp.java
@@ -3,6 +3,7 @@ package com.luv2code.aopdemo;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import com.luv2code.aopdemo.dao.AccountDAO;
+import com.luv2code.aopdemo.dao.MembershipDAO;
 
 public class MainDemoApp {
 
@@ -13,8 +14,14 @@ public class MainDemoApp {
 //		get bean from the container
 		AccountDAO account = context.getBean("accountDAO", AccountDAO.class);
 		
+//		get bean from the container
+		MembershipDAO member = context.getBean("membershipDAO", MembershipDAO.class);
+		
 //		call the business method
 		account.addAccount();
+		
+//		call the business method
+		member.addAccount();
 		
 //		close the context
 		context.close();
