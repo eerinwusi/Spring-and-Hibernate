a9a27ae998852fddc37c086775f51a7278de3c62
Emmanuel Erinwusi <Emmanuel.Erinwusi@etranzact.com>
2022-09-30 14:53:21 +0100

    Ordering Aspects

--- spring-aop/src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ spring-aop/src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -2,48 +2,32 @@ package com.luv2code.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
+@Order(2)
 public class MyDemoLoggingAspect {
 	
 //	this is where we add all of our related advices for logging
 	
 //	let's start with an @Before advice
 	
-	@Pointcut("execution(* com.luv2code.aopdemo.dao.*.*(..))")
-	public void forDaoPackage() {}
-	
-	@Before("forDaoPackage()")
-	public void beforeAddAccountAdvice1() {
-		System.out.println("\n=======>>> Executing @Before advice on addAccount()");
-	}
-	
-	@Before("forDaoPackage()")
-	public void performApiAnalytics1() {
-		System.out.println("\n=====> Performing API analytics");
-	}
-	
-//	create pointcut for getter methods
-	@Pointcut("execution(* com.luv2code.aopdemo.dao.*.get*(..))")
-	public void getter() {}
-	
-//	create pointcut for setter methods
-	@Pointcut("execution(* com.luv2code.aopdemo.dao.*.set*(..))")
-	public void setter() {}
-	
-	@Pointcut("forDaoPackage() && !(getter() || setter())")
-	private void forDaoPackageNoGetterSetter() {}
+//	@Before("forDaoPackage()")
+//	public void beforeAddAccountAdvice1() {
+//		System.out.println("\n=======>>> Executing @Before advice on addAccount()");
+//	}
+//	
+//	@Before("forDaoPackage()")
+//	public void performApiAnalytics1() {
+//		System.out.println("\n=====> Performing API analytics");
+//	}
 
-	@Before("forDaoPackageNoGetterSetter()")
+	@Before("com.luv2code.aopdemo.aspect.LuvAopExpressions.forDaoPackageNoGetterSetter()")
 	public void beforeAddAccountAdvice() {
 		System.out.println("\n====> Executing @Before advice on method");
 	}
 	
-	@Before("forDaoPackageNoGetterSetter()")
-	public void performApiAnalytics() {
-		System.out.println("\n====> Performing API analytics");
-	}
+	
 }
