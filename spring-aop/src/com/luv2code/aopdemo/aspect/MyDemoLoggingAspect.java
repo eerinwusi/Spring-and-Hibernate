a5a54cc892a115912e4cad9309d2abb7c595ec0c
Emmanuel Erinwusi <Emmanuel.Erinwusi@etranzact.com>
2022-09-30 12:53:11 +0100

    Pointcut Declarations

--- spring-aop/src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ spring-aop/src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -2,6 +2,7 @@ package com.luv2code.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -12,9 +13,17 @@ public class MyDemoLoggingAspect {
 	
 //	let's start with an @Before advice
 	
-	@Before("execution(* com.luv2code.aopdemo.dao.*.*(..))")
+	@Pointcut("execution(* com.luv2code.aopdemo.dao.*.*(..))")
+	public void forDaoPackage() {}
+	
+	@Before("forDaoPackage()")
 	public void beforeAddAccountAdvice() {
 		System.out.println("\n=======>>> Executing @Before advice on addAccount()");
+	}
+	
+	@Before("forDaoPackage()")
+	public void performApiAnalytics() {
+		System.out.println("\n=====> Performing API analytics");
 	}
 
 }
