3989cd79476544dca15e91c3f92097a6069b4d0b
Emmanuel Erinwusi <Emmanuel.Erinwusi@etranzact.com>
2022-09-30 14:05:57 +0100

    Combining pointcuts 2

--- spring-aop/src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ spring-aop/src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -17,13 +17,33 @@ public class MyDemoLoggingAspect {
 	public void forDaoPackage() {}
 	
 	@Before("forDaoPackage()")
-	public void beforeAddAccountAdvice() {
+	public void beforeAddAccountAdvice1() {
 		System.out.println("\n=======>>> Executing @Before advice on addAccount()");
 	}
 	
 	@Before("forDaoPackage()")
-	public void performApiAnalytics() {
+	public void performApiAnalytics1() {
 		System.out.println("\n=====> Performing API analytics");
 	}
+	
+//	create pointcut for getter methods
+	@Pointcut("execution(* com.luv2code.aopdemo.dao.*.get*(..))")
+	public void getter() {}
+	
+//	create pointcut for setter methods
+	@Pointcut("execution(* com.luv2code.aopdemo.dao.*.set*(..))")
+	public void setter() {}
+	
+	@Pointcut("forDaoPackage() && !(getter() || setter())")
+	private void forDaoPackageNoGetterSetter() {}
 
+	@Before("forDaoPackageNoGetterSetter()")
+	public void beforeAddAccountAdvice() {
+		System.out.println("\n====> Executing @Before advice on method");
+	}
+	
+	@Before("forDaoPackageNoGetterSetter()")
+	public void performApiAnalytics() {
+		System.out.println("\n====> Performing API analytics");
+	}
 }
