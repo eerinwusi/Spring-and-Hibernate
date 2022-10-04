0a5106a9a6abdd286c5a0b663ac5fac0b722b5b6
Emmanuel Erinwusi <Emmanuel.Erinwusi@etranzact.com>
2022-10-04 11:41:22 +0100

    @AfterReturning 3

--- spring-aop/src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ spring-aop/src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -1,6 +1,9 @@
 package com.luv2code.aopdemo.aspect;
 
+import java.util.List;
+
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.reflect.MethodSignature;
@@ -27,6 +30,22 @@ public class MyDemoLoggingAspect {
 //	public void performApiAnalytics1() {
 //		System.out.println("\n=====> Performing API analytics");
 //	}
+	
+	
+//	add a new advice for @AfterReturning
+	
+	@AfterReturning(
+			pointcut="execution(* com.luv2code.aopdemo.dao.AccountDAO.findAccounts(..))",
+			returning="result")
+	public void afterReturningFindAccountsAdvice(JoinPoint joinPoint, List<Account> result) {
+		
+//		print out which method we are advising on
+		String method = joinPoint.getSignature().toShortString();
+		System.out.println("\n======>>> Executing @AfterReturning on method: " + method);
+		
+//		print out the results of the method call
+		System.out.println("\n=======>>> result is " + result);
+	}
 
 	@Before("com.luv2code.aopdemo.aspect.LuvAopExpressions.forDaoPackageNoGetterSetter()")
 	public void beforeAddAccountAdvice(JoinPoint theJoinPoint) {
