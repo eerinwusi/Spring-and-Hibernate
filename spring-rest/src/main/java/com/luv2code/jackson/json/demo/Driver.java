fd386b57fa75ce18031da41f5cd838969dac3937
Emmanuel Erinwusi <Emmanuel.Erinwusi@etranzact.com>
2022-10-21 12:43:48 +0100

    Spring Rest - JSON Jackson Demo - Nested Objects and Arrays

--- spring-rest/src/main/java/com/luv2code/jackson/json/demo/Driver.java
+++ spring-rest/src/main/java/com/luv2code/jackson/json/demo/Driver.java
@@ -16,11 +16,22 @@ public class Driver {
 			// data/sample-lite.json
 			
 			Student theStudent = mapper.readValue(
-						new File("data/sample-lite.json"), Student.class);
+						new File("data/sample-full.json"), Student.class);
 			
 			// print first name and last name
 			System.out.println("First name = " + theStudent.getFirstName());
 			System.out.println("Last name = " + theStudent.getLastName());
+			
+//			print out address: street and city
+			Address address = theStudent.getAddress();
+			
+			System.out.println("Street: " + address.getStreet());
+			System.out.println("City: " + address.getCity());
+			
+//			print out languages array
+			for (String lang : theStudent.getLanguages()) {
+				System.out.println(lang);
+			}
 
 		}
 		catch (Exception exc) {
